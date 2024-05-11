2cdc19d822ab396b4c7e32f596609e06a5d1d2aa
Vibhav Ucharia <vibhav09112003@gmail.com>
2024-05-11 20:52:07 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,30 @@
+# When you want to upload from program as a package
+import setuptools
+
+with open('README.md', "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "Vibhav911"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "vibhav09112003@gmail.com"
+
+
+setuptools.setup(
+    name= SRC_REPO,
+    version= __version__,
+    author= AUTHOR_USER_NAME,
+    author_email= AUTHOR_EMAIL,
+    description= "A small package for text summarization (NLP) app",
+    long_description= long_description,
+    long_description_content = "text/markdown",
+    url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url= {
+        "Bug_Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir= {"": "src"},
+    packages=setuptools.find_packages(where= "src")
+)
